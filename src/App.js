3330cd80f24092a5cb220dc19560fa2aa6de5f43
Fabrizio Montoya <fabriziorandall@gmail.com>
2022-03-27 14:54:52 -0500

    Barra navegacion

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import BarraNavegacion from "./Componentes/Navbar";
+
+function App() {
+  return <BarraNavegacion></BarraNavegacion>;
+}
+
+export default App;
